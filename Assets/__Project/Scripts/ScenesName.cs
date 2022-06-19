53b5f60e5e17dd08b3eeb5eef849b9500dd386c3
Rafael Briet <rafael-briet@hotmail.com>
2022-06-19 16:17:13 -0300

    Add main menu scene

---
+++ Assets/__Project/Scripts/ScenesName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvadersRemake
+{
+    public static class ScenesName
+    {
+        public const string Game = "Game";
+        public const string MainMenu = "MainMenu";
+    }
+}
